da9e9c5472a40e90294185db69734014713e11f5
Kaplaugher <kaplaugher94@gmail.com>
2017-11-03 17:34:35 -0400

    map and descriptions

--- site/themes/hugo-elate-theme/static/js/google_map.js
+++ site/themes/hugo-elate-theme/static/js/google_map.js
@@ -5,13 +5,13 @@ function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(51.0825523,3.574402,11);
+    var myLatlng = new google.maps.LatLng(34.075375,-84.294090,11);
     // 39.399872
     // -8.224454
     
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
-        zoom: 7,
+        zoom: 9,
 
         // The latitude and longitude to center the map (always required)
         center: myLatlng,
